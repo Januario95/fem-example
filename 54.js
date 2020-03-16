49eac61b9d22ee7d2ad543c48e8a573af9661474
= <januariocipriano@gmail.com>
2020-03-16 18:08:47 +0800

    initial commig for the calculator

---
+++ 54.js
@@ -0,0 +1,24 @@
+"use strict";
+
+ls
+ls -a 
+cd 
+clear
+ls -la 
+ls -lah
+clear
+man ls
+touch index.html // create a new index.html file
+echo "<h1>Hi this is cool</h1>" > index.html // put the tag into the file
+cat index.html // read content from the file
+vi index.html // to edit file in vim
+type I for start editing
+press Esc to exit edit more
+:w - save
+:q - quit
+:wq - save and quit
+
+git init
+
+
+
